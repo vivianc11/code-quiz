6795e28617c1679d44c991b7e373f78450ac84e0
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-05 12:33:18 -0700

    worked on functions in JS that displays the intro card, displays the individual questions one at a time, and iterating through the questions after the user selects an answer

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 // variables targeted in HTML
 var introCard = document.querySelector(".intro-card");
-var start = document.querySelector(".start");
+var h1 = document.querySelector(".h1");
+var about = document.querySelector(".about");
+var start = document.querySelector(".start-button");
 var timeCounter = document.querySelector(".timer-count");
 var questionInfo = document.querySelector(".question-info");
 var answers = document.querySelector(".answers");
@@ -9,51 +11,90 @@ var initials = document.querySelector("#initials");
 var highScore = document.querySelector(".highscore");
 var goBack = document.querySelector(".go-back");
 var reset = document.querySelector(".reset");
+const startBtn = document.createElement('button');
+var option1Li = document.createElement('li');
+var option2Li = document.createElement('li');
+var option3Li = document.createElement('li');
+var option4Li = document.createElement('li');
+var button1 = document.createElement('button');
+var button2 = document.createElement('button');
+var button3 = document.createElement('button');
+var button4 = document.createElement('button');
 
+h1.textContent = "";
 var timer;
 var timerCount;
 var correct;
 var incorrect;
+var index = 0;
+
+var introCard = [
+    {title: "Coding Quiz Challenge",
+    about: "Please answer the following 3 coding questions to the best of your abilities. You will have 75 seconds to complete the 3 questions. Good Luck!!"
+    }
+]
 
 var codingQuestions = [
-    {question1: Blah,
+    {question: "Blah",
     answers: {
-        a: a,
-        b: b,
-        c: c,
-        d: d,
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
     },
-    correctAnswer: b },
-
-    {question2: Blah,
-        answers: {
-            a: a,
-            b: b,
-            c: c,
-            d: d,
-        },
-    correctAnswer: b },
-
-    {question3: Blah,
-        answers: {
-            a: a,
-            b: b,
-            c: c,
-            d: d,
-        },
-        correctAnswer: b },
-
+    correctAnswer: "b" },
+    {question: "Blah2",
+    answers: {
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+    },
+    correctAnswer: "b" },
+    {question: "Blah3",
+    answers: {
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+    },
+    correctAnswer: "b" },
+  
 ]
-// Generating the quiz
-function startQuiz(){
-    timer = 100;
-    document.innerHTML = ""
-    // looping through questions to display them
-    for (let index = 0; index < codingQuestions.length; index++) {
-        
-        
-    }
 
+//Generating intro card
+function Introduction() {
+    h1.textContent = introCard[0].title;
+    about.textContent = introCard[0].about;
+    startBtn.textContent = "Start Quiz";
+    start.appendChild(startBtn);  
+}
+
+// Generating the quiz
+function startQuiz() {
+    timer = 75;
+    h1.textContent = "";
+    about.textContent = "";
+    start.removeChild(startBtn);
+    // display first question
+    displayQ();
+}
+
+// Function to display questions one at a time
+function displayQ() {
+    questionInfo.textContent = codingQuestions[index].question;
+    answers.appendChild(option1Li);
+    answers.appendChild(option2Li);
+    answers.appendChild(option3Li);
+    answers.appendChild(option4Li);
+    option1Li.appendChild(button1);
+    option2Li.appendChild(button2);
+    option3Li.appendChild(button3);
+    option4Li.appendChild(button4);
+    button1.textContent = codingQuestions[index].answers.a;
+    button2.textContent = codingQuestions[index].answers.b;
+    button3.textContent = codingQuestions[index].answers.c;
+    button4.textContent = codingQuestions[index].answers.d;
 }
 
 // Timer Function
@@ -70,3 +111,10 @@ function startTimer() {
 
 //eventlisteners
 start.addEventListener("click", startQuiz);
+
+answers.addEventListener("click", () => {
+    if (index < 2) {
+        index++;
+        displayQ();
+    }
+})
